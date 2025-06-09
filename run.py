77f3d841d796c41e01633e03afe439b87442626e
Filip Nowicki <s464915@g1n5.cluster.wmi.amu.edu.pl>
2025-06-09 09:19:03 +0200

    fix: run

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 import uvicorn
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=8000, help="Port to run the API on")
+    parser.add_argument("--host", type=str, default="0.0.0.0", help="Host to run the API on")
+
+    args = parser.parse_args()
     uvicorn.run(
         "app.main:app",  
         host="0.0.0.0",
